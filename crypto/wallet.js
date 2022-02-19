71f3f278fff79df5e04c1b7ccb78ce2277378bb1
eoinco <eoin.connolly@gmail.com>
2022-02-19 17:21:32 +0000

    created async function

--- crypto/wallet.js
+++ crypto/wallet.js
@@ -3,4 +3,22 @@
 
 // using BIP39, BIP44
 
-console.log("this is the wallet creation file")
+
+// to generate an ETH address
+// first: an secp256k1 keypair
+// then we take the public key
+// keccak256 hash it
+// drop the first 12 bytes
+
+
+// load the dependencies
+const Wallet = require("ethereumjs-wallet").default;
+const keccak256 = require("keccak256");
+
+const getWalletDetails = async() => {
+    console.log("async function started.")
+}
+
+getWalletDetails();
+
+console.log("this is the wallet creation file");
