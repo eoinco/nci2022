dc79cd4966126d13fbdc41510b53ef1f01deec2d
eoinco <eoin.connolly@gmail.com>
2022-02-19 17:57:16 +0000

    final tidy up

--- crypto/wallet.js
+++ crypto/wallet.js
@@ -38,20 +38,18 @@ const getWalletDetails = async() => {
 
 const getHDWalletDetails = async() => {
     console.log("making a hd wallet");
-    const mnemonic = "this is your super secret seed phrase";
+    const mnemonic = "super secret seed phrase";
 
     let seed = await bip39.mnemonicToSeed(mnemonic);
 
     let hdwallet = hdkey.fromMasterSeed(seed);
-    const path  = "m/44'/60'/0'/0/0";
-    const path2 = "m/44'/60'/0'/0/1";
+    const path  = "m/44'/60'/0'/0/0"; // account 2 in metamask is "m/44'/60'/0'/0/1";
     const wallet = hdwallet.derivePath(path).getWallet();
-    console.log(`wallet is ${wallet}`);
 
     const pubKey  = wallet.getPublicKey();
     const hashedPubKey = keccak256(pubKey).toString('hex');
-    console.log(`hashed public key is: 0x${hashedPubKey}`);
     const ethAddress = hashedPubKey.substring(24);
+    
     console.log(`eth address is: 0x${ethAddress}`);
 }
 
