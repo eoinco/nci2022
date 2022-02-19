b9010d2a7c5371fe5953f136ec739f473b0f55bc
eoinco <eoin.connolly@gmail.com>
2022-02-19 17:14:45 +0000

    updated wallet

---
+++ crypto/wallet.js
@@ -0,0 +1,6 @@
+// in our metamask, we have a number of ethereum addresses
+// this code will show you how to create those addresses
+
+// using BIP39, BIP44
+
+console.log("this is the wallet creation file")
