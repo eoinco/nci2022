95db4519d893c7130e658d48aaddf3451455f108
eoinco <eoin.connolly@gmail.com>
2022-02-19 17:52:27 +0000

    about to repro metamask

--- crypto/wallet.js
+++ crypto/wallet.js
@@ -11,6 +11,10 @@
 const Wallet = require("ethereumjs-wallet").default;
 const keccak256 = require("keccak256");
 
+const bip39 = require("bip39");
+const { hdkey } = require("ethereumjs-wallet");
+
+
 const getWalletDetails = async() => {
     console.log("async function started.");
     
@@ -27,14 +31,28 @@ const getWalletDetails = async() => {
 
     const ethAddress = hashedPubKey.substring(24);
     console.log(`eth address is: 0x${ethAddress}`);
-    const privKey = wallet.getPrivateKey();
+    //const privKey = wallet.getPrivateKey();
 }
 
 // deterministic wallet
 
 const getHDWalletDetails = async() => {
     console.log("making a hd wallet");
-    
+    const mnemonic = "this is your super secret seed phrase";
+
+    let seed = await bip39.mnemonicToSeed(mnemonic);
+
+    let hdwallet = hdkey.fromMasterSeed(seed);
+    const path  = "m/44'/60'/0'/0/0";
+    const path2 = "m/44'/60'/0'/0/1";
+    const wallet = hdwallet.derivePath(path).getWallet();
+    console.log(`wallet is ${wallet}`);
+
+    const pubKey  = wallet.getPublicKey();
+    const hashedPubKey = keccak256(pubKey).toString('hex');
+    console.log(`hashed public key is: 0x${hashedPubKey}`);
+    const ethAddress = hashedPubKey.substring(24);
+    console.log(`eth address is: 0x${ethAddress}`);
 }
 
 //getWalletDetails();
