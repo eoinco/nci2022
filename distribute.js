076907e7642f13fe813392b3b77e9cb39d3017cd
eoinco <eoin.connolly@gmail.com>
2022-03-05 17:24:06 +0000

    added distribute and accounts

---
+++ distribute.js
@@ -0,0 +1,7 @@
+
+// read in a file of accounts
+// count the number of accounts
+// get the remaining supply belonging to token owner
+// divide 5% of remaining supply between the accounts
+// collect tea and medals
+
