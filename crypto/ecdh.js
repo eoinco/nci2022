cf2c3e282ac09806eb1c60a8b8ffd33d605ef0b6
eoinco <eoin.connolly@gmail.com>
2022-04-02 17:03:47 +0100

    added ecdh file

---
+++ crypto/ecdh.js
@@ -0,0 +1,13 @@
+// for ECDH
+// Alice has a private key and a public key (elliptic curve)
+// Bob has a private key and a public key
+
+// Alice gives Bob her public key
+// Bob gives Alice his public key
+
+// Somehow, magically, Alice and Bob will then 
+// agree on the same secret,
+// which they can then use to encrypt communications
+
+// this is a KEY EXCHANGE
+
