34203151cbd16138489bd84ffac6811af0a2dbf0
eoinco <eoin.connolly@gmail.com>
2022-04-25 16:20:28 +0100

    added todo

---
+++ crypto/encrypt.js
@@ -0,0 +1,8 @@
+var sodium = require("sodium-native");
+
+// create a message
+// get a secret
+// encrypt the message with the secret
+// decrypt the message with the secret
+// hopefully the decrypted message is the same as the original message
+
