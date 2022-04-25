1eb60d4e6b5892698855d112d67fbe90882a946b
eoinco <eoin.connolly@gmail.com>
2022-04-25 14:39:17 +0100

    enabled distro via handlers

--- handlers.js
+++ handlers.js
@@ -18,7 +18,24 @@
 const express = require('express');
 const app = express()
 app.use(express.json())
-
 const port = 8080;
+
+
+let contract = require('./contract.js');
+
+app.get('/symbol', async(req, res) => {
+    res.send(await contract.getSymbol());
+})
+
+app.get('/name', async(req, res) => {
+    res.send(await contract.getName());
+})
+
+let distro = require('./distribute.js');
+app.get('/distro', async(req, res) => {
+    res.send(await distro.doDistro());
+})
+
+
 app.listen(port, () => console.log(`listening on port ${port}...`));
 
