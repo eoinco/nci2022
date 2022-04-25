eb6454651ecf1e48059daab59c874da218d7d2da
eoinco <eoin.connolly@gmail.com>
2022-04-25 15:24:19 +0100

    created keypair

--- crypto/sign.js
+++ crypto/sign.js
@@ -15,13 +15,25 @@ var sodium = require('sodium-native');
 const pubkeybytes = sodium.crypto_sign_PUBLICKEYBYTES;
 const privkeybytes = sodium.crypto_sign_SECRETKEYBYTES;
 
-
 console.log(`pub key bytes: ${pubkeybytes}`);
 console.log(`secret key bytes: ${privkeybytes}`);
 
+const alicePublicSigningKey = sodium.sodium_malloc(pubkeybytes);
+const alicePrivateSigningKey = sodium.sodium_malloc(privkeybytes);
+
+sodium.sodium_memzero(alicePublicSigningKey);
+sodium.sodium_memzero(alicePrivateSigningKey);
+
+sodium.crypto_sign_keypair(alicePublicSigningKey, alicePrivateSigningKey);
+
+console.log(`alice public key: ${alicePublicSigningKey.toString('hex')}`);
+
 // create a message
 
+const message = "the woods are dark and deep";
+
 // hash the message
+
 
 // sign with alice private key
 
