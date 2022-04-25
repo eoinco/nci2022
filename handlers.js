bbefb7f232006ca953e21499259c7d2af8e79b50
eoinco <eoin.connolly@gmail.com>
2022-04-25 14:19:41 +0100

    working web server

--- handlers.js
+++ handlers.js
@@ -16,4 +16,9 @@
 // CRUD (create, read, update, delete)
 
 const express = require('express');
+const app = express()
+app.use(express.json())
+
+const port = 8080;
+app.listen(port, () => console.log(`listening on port ${port}...`));
 
