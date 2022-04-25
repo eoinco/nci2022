be1306879338ef8de7b14bf7dcf05dd5a01eb083
eoinco <eoin.connolly@gmail.com>
2022-04-25 15:10:22 +0100

    added todo

--- crypto/sign.js
+++ crypto/sign.js
@@ -1,3 +1,21 @@
 // in here, we are going to sign and verify 
 // using the x25519 elliptic curve algo
 
+// SIGN with the private key
+// VERIFY with the public key
+
+// we usually sign a HASH of a message
+// not the message
+
+// use C25519 to sign/verify
+
+
+// create an alice key pair
+
+// create a message
+
+// hash the message
+
+// sign with alice private key
+
+// verify with alice pub key
