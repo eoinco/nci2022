6731ff6dc53dcee90ef3bb686febabdc84ced698
eoinco <eoin.connolly@gmail.com>
2022-04-25 14:17:00 +0100

    mo handlers

---
+++ handlers.js
@@ -0,0 +1,19 @@
+// we're going to create a web server
+// that will allow us to:
+// upload your code to a computer in the cloud
+// then people can interact with it
+// using a front end
+
+
+// rest api
+
+
+// GET - reads
+// POST - writes
+// PUT - updates
+// DELETE - removes
+
+// CRUD (create, read, update, delete)
+
+const express = require('express');
+
